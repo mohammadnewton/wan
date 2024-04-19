65b82fca693148f92c25007f7cec09915c6d81d8
Newton Bofulo <newtonombese1@gmail.com>
2024-04-19 18:34:45 +0300

    Add font family configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poly': ['Poly', 'serif'],
+        'inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
