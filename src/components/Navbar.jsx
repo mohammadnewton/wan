402926474d7eecca631b40fd8b05d803ff64e45a
Newton Bofulo <newtonombese1@gmail.com>
2024-04-19 09:27:29 +0300

    Successful setup of the hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="flex justify-evenly items-center px-8 bg-white text-black h-[84px] border">
-      <ul className={`flex items-center space-x-16`}>
+      <ul className={`flex items-center space-x-16 ${isOpen ? 'hidden' : 'lg:flex'}`}>
         <li className="navbar-link">
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link" onClick={() => setIsOpen(false)}>
             Home
           </Link>
         </li>
@@ -15,7 +20,7 @@ const Navbar = () => {
           <Link
             to="/transformations"
             className="nav-link"
-           
+            onClick={() => setIsOpen(false)}
           >
             Transformations
           </Link>
@@ -28,18 +33,38 @@ const Navbar = () => {
           className="w-20 h-auto"
         />
       </div>
-      <ul className={`flex items-center space-x-16`}>
+      <ul className={`flex items-center space-x-16 ${isOpen ? 'lg:hidden' : 'lg:flex'}`}>
         <li className="navbar-link">
-          <Link to="/services" className="nav-link">
+          <Link to="/services" className="nav-link" onClick={() => setIsOpen(false)}>
             Services
           </Link>
         </li>
         <li className="navbar-link">
-          <Link to="/contact" className="nav-link">
+          <Link to="/contact" className="nav-link" onClick={() => setIsOpen(false)}>
             Contact
           </Link>
         </li>
       </ul>
+      <button
+        className={`lg:hidden ${isOpen ? 'fixed right-4' : 'absolute right-4'}`}
+        onClick={toggleMenu}
+        aria-label={isOpen ? "Close navigation" : "Open navigation"}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+          />
+        </svg>
+      </button>
     </nav>
   );
 };
