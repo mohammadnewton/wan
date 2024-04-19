eac14acd85bcc78f8183953d129d9c4587522b37
Newton Bofulo <newtonombese1@gmail.com>
2024-04-19 09:05:28 +0300

    Setting up a dummy logo for testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-evenly items-center py-4 px-8 bg-white text-black">
+    <nav className="flex justify-evenly items-center px-8 bg-white text-black h-[84px] border">
       <ul className="flex items-center space-x-16">
         <li className="navbar-link">
           <Link to="/" className="nav-link">
@@ -17,7 +17,7 @@ const Navbar = () => {
       </ul>
       <div className="navbar-logo">
         <img
-          src="your-logo-url"
+          src="https://images.unsplash.com/photo-1496200186974-4293800e2c20?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bG9nb3xlbnwwfDB8MHx8fDA%3D"
           alt="Company Logo"
           className="w-20 h-auto"
         />
