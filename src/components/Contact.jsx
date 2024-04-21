4506fc93fe335a942dab859051a429b427938529
Newton Bofulo <newtonombese1@gmail.com>
2024-04-21 06:48:00 +0300

    Implement the UI of the contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,74 @@
+
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="text">
+        <h3>CONTACT US</h3>
+        <h2>
+          Let&apos;s talk about <br />
+          Love to hear from you
+        </h2>
+      </div>
+      <div className="contact-us">
+        <h3>Send us a Message</h3>
+        <form action="#">
+          <label>Full Name<em>&#x2a;</em></label
+          ><input
+            id=""
+            name=""
+            required=""
+            type="text"
+            placeholder="Adam Gelius"
+          />
+          <label>Email<em>&#x2a;</em></label
+          ><input
+            id=""
+            name=""
+            required=""
+            type="email"
+            placeholder="example@yourmail.com"
+          />
+          <label>Phone</label
+          ><input
+            id=""
+            name=""
+            type="tel"
+            required = ""
+            placeholder="+885 1254 5211 552"
+          />
+          <label>Message<em>&#x2a;</em></label
+          ><input
+          id=""
+          name=""
+          required=""
+          type="text"
+          placeholder="type your message here"
+        /><button
+            id="customerOrder"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+      <div className="social">
+        <div className="location">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="detail">
+            <h3>Our Location</h3>
+            <p>401 Broadway, 24th Floor, Orchard Cloud View, London</p>
+          </div>
+        </div>
+        <div className="mail">
+          <i className="fa-regular fa-envelope"></i>
+          <div className="detail">
+            <h3>How Can We Help?</h3>
+            <p>info@yourdomain.com <br />contact@yourdomain.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
